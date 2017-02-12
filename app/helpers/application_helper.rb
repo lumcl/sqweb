2d980cc5d8b72d5ba9487268cb4298832d7f9072
lum <ibmt20@hotmail.com>
2017-02-12 11:11:13 +0800

    Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
 
+  def export_to_excel(table_id)
+    #<a download="report.xls" href="#" onclick="return ExcellentExport.excel(this, 'rpt', 'Sheet');">Excel</a>
+    link_to 'Excel', '#', download:'report.xls', onclick:"return ExcellentExport.excel(this, '#{table_id}', 'Sheet');"
+  end
+
 end
